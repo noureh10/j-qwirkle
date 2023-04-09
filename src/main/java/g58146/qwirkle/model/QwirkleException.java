1fcfcd5d4ba06ae597cf740c8b41cbb029be5c5e
Nour Echaara <58146@etu.he2b.be>
2023-04-09 18:34:18 +0200

    Adding the QwirkleException class that extends RuntimeException

---
+++ src/main/java/g58146/qwirkle/model/QwirkleException.java
@@ -0,0 +1,10 @@
+package g58146.qwirkle.model;
+/**
+ *
+ * @author Nour
+ */
+public class QwirkleException extends RuntimeException{
+    public QwirkleException(String message){
+        super(message);
+    }
+}
