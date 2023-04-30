70f05267bab7cea8c91016a7c99909a6cb7c80ec
Nour Echaara <58146@etu.he2b.be>
2023-04-30 09:19:20 +0200

    Updating the GridTest class so it includes all the moves in the rulebook

--- src/test/java/g58146/qwirkle/model/GridTest.java
+++ src/test/java/g58146/qwirkle/model/GridTest.java
@@ -14,13 +14,14 @@ import static g58146.qwirkle.model.QwirkleTestUtils.*;
 
 class GridTest {
     private Grid grid;
+    private Grid modGrid;
     
 
     @BeforeEach
     void setUp() {
         grid = new Grid();
     }
-
+    
     @Test
     void firstAdd_one_tile() {
         var tile = new Tile(BLUE, CROSS);
@@ -38,7 +39,7 @@ class GridTest {
         assertEquals(t2, get(grid, -1, 0));
         assertEquals(t3, get(grid, -2, 0));
     }
-
+    
     @Test
     void rules_sonia_a_adapted_to_fail() {
         var t1 = new Tile(RED, ROUND);
@@ -91,5 +92,116 @@ class GridTest {
         var g = new Grid();
         assertDoesNotThrow(() -> get(g, -250, 500));
         assertNull(get(g, -250, 500));
+    }
+    // Test for the rules
+    @Test
+    void sonia_move_1(){
+        this.modGrid = this.grid;
+        //Adding all the tiles
+        addTile(modGrid, 0, 0, Color.RED, Shape.PLUS);
+        addTile(modGrid, 1, 0, Color.RED, Shape.DIAMOND);
+        addTile(modGrid, 2, 0, Color.RED, Shape.ROUND);
+        addTile(modGrid, 3, 0, Color.RED, Shape.SQUARE);
+        addTile(modGrid, -1, -1, Color.GREEN, Shape.STAR);
+        addTile(modGrid, 0, -1, Color.GREEN, Shape.PLUS);
+        addTile(modGrid, 1, -1, Color.GREEN, Shape.DIAMOND);
+        addTile(modGrid, 2, -1, Color.GREEN, Shape.ROUND);
+        addTile(modGrid, 2, 1, Color.BLUE, Shape.ROUND);
+        addTile(modGrid, 3, 1, Color.BLUE, Shape.SQUARE);
+        addTile(modGrid, 3, 2, Color.PURPLE, Shape.SQUARE);
+    }
+    @Test
+    void sonia_move_1_fail(){
+        sonia_move_1();
+        assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,2,-2,Color.GREEN,Shape.ROUND));
+        assertNull(get(grid,2,-2));
+    }
+    @Test
+    void cedric_move_1(){
+        sonia_move_1();
+        addTile(modGrid,3,3,Color.ORANGE,Shape.SQUARE);
+        addTile(modGrid,4,3,Color.RED,Shape.SQUARE);
+    }
+    @Test
+    void cedric_move_1_fail(){
+        cedric_move_1();
+        assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,1,1,Color.YELLOW,Shape.DIAMOND));
+    }
+    @Test
+    void elvire_move_1(){
+        cedric_move_1();
+        addTile(modGrid,-1,-2,Color.YELLOW,Shape.STAR);
+        addTile(modGrid,-1,-3,Color.ORANGE,Shape.STAR);
+    }
+    @Test
+    void elvire_move_fail(){
+        elvire_move_1();
+        assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,0,-2,Color.BLUE,Shape.DIAMOND));
+    }
+    @Test
+    void vincent_move_1(){
+        elvire_move_1();
+        addTile(modGrid,0,-3,Color.ORANGE,Shape.CROSS);
+        addTile(modGrid,1,-3,Color.ORANGE,Shape.DIAMOND);
+        
+    }
+    @Test
+    void vincent_move_1_fail(){
+        vincent_move_1();
+        assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,1,-2,Color.YELLOW,Shape.ROUND));
+    }
+    @Test 
+    void sonia_move_2(){
+        vincent_move_1();
+        addTile(modGrid,1,-2,Color.YELLOW,Shape.DIAMOND);
+        addTile(modGrid,2,-2,Color.YELLOW,Shape.ROUND);
+    }
+    @Test
+    void sonia_move_2_fail(){
+        sonia_move_2();
+        assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,2,-3,Color.PURPLE,Shape.ROUND));
+    }
+    @Test
+    void cedric_move_2(){
+        sonia_move_2();
+        addTile(modGrid,-1,0,Color.RED,Shape.STAR);
+    }
+    @Test
+    void cedric_move_2_fail(){
+        cedric_move_2();
+         assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,0,-2,Color.PURPLE,Shape.PLUS));
+    }
+    @Test
+    void elvire_move_2(){
+        cedric_move_2();
+        addTile(modGrid,4,-1,Color.ORANGE,Shape.CROSS);
+        addTile(modGrid,4,0,Color.RED,Shape.CROSS);
+        addTile(modGrid,4,1,Color.BLUE,Shape.CROSS);
+    }
+    @Test
+    void elvire_move_2_fail(){
+        elvire_move_2();
+        addTile(modGrid,2,4,Color.PURPLE,Shape.CROSS);
+    }
+    @Test
+    void vincent_move_2(){
+        elvire_move_2();
+        addTile(modGrid,3,4,Color.YELLOW,Shape.SQUARE);
+        addTile(modGrid,4,4,Color.BLUE,Shape.SQUARE);
+    }
+    @Test
+    void vincent_move_2_fail(){
+        vincent_move_2();
+         assertThrows(QwirkleException.class, () -> 
+                addTile(modGrid,2,4,Color.YELLOW,Shape.CROSS));
+    }
+    private void addTile(Grid grid,int row,int col,Color c, Shape s){
+        grid.add(createTileAtpos(row, col, new Tile(c, s)));
     }
 }
