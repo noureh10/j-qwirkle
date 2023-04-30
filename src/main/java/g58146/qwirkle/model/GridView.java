5454efaf1e9e00a17c8aa867aeeff3c3e7a4cce0
Nour Echaara <58146@etu.he2b.be>
2023-04-30 13:28:36 +0200

    Adding the GridView class

---
+++ src/main/java/g58146/qwirkle/model/GridView.java
@@ -0,0 +1,27 @@
+package g58146.qwirkle.model;
+/**
+ *
+ * @author Nour
+ */
+public class GridView {
+    private Grid grid;
+    /**
+     * Constructor of the grid view class
+     * @param grid 
+     */
+    public GridView(Grid grid){
+        this.grid = grid;
+    }
+    /**
+     * Returns a tile from a desired row and column
+     */
+    public Tile get(int row,int col){
+        return grid.get(row, col);
+    }
+    /**
+     * Checks if the grid is empty
+     */
+    public boolean isEmpty(){
+        return grid.isEmpty();
+    }
+}
