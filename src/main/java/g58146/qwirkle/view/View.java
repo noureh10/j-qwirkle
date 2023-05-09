98f3403f9feecceb2547ecdca3aca84b8d38b424
Nour Echaara <58146@etu.he2b.be>
2023-05-09 15:01:56 +0200

    Itération 1 - Modification de la classe vue pour afficher les tuiles du joueurs, affichers des messages sans inline et ajout d'un prompt dans le displayHelp pour réafficher les commandes

--- src/main/java/g58146/qwirkle/view/View.java
+++ src/main/java/g58146/qwirkle/view/View.java
@@ -1,5 +1,6 @@
 package g58146.qwirkle.view;
 import g58146.qwirkle.model.*;
+import java.util.List;
 /**
  *
  * @author Nour
@@ -10,8 +11,13 @@ public class View{
      * @param player
      */
     public static void display(Player player){
-         System.out.println("Player's name : " + player.getName());
-         System.out.println("Player's hand : " + player.getHand());
+        System.out.println("Player's name : " + player.getName());
+        List<Tile> playerHand = player.getHand();
+        System.out.println("Player's hand : ");
+        for (Tile tile : playerHand) {
+            System.out.print(tileColor(tile)+tileShape(tile) + " ");
+        }
+        System.out.print("\u001B[39m");;
     }
     /**
      * This method diplays the help prompt
@@ -24,12 +30,12 @@ public class View{
                 - play line : l <row> <col> <direction> <i1> [<i2>]
                 - play plic-ploc : m <row1> <col1> <i1> [<row2> <col2> <i2>]
                 - play first : f <i1> [<i2>]
+                - help : h
                 - pass : p
                 - quit : q
                 i : index in list of tiles
                 d : direction in l (left), r (right), u (up), d(down)""");
     }
-
     /**
      * This method displays the grid. It first find the minimum and maximum rows and column with tiles on them, and it then
      * displays it.
@@ -37,8 +43,8 @@ public class View{
      */
     public static void display(GridView grid) {
         // Find the minimum and maximum rows and columns with tiles on them
-        int rowMin = Integer.MAX_VALUE, rowMax = Integer.MIN_VALUE;
-        int colMin = Integer.MAX_VALUE, colMax = Integer.MIN_VALUE;
+        int rowMin = 91, rowMax = 0;
+        int colMin = 91, colMax = 0;
         for (int i = 0; i < 91; i++) {
             for (int j = 0; j < 91; j++) {
                 if (grid.getGrid().get(i, j) != null) {
@@ -74,8 +80,15 @@ public class View{
      * This method displays error messages.
      * @param message
      */
-    public void displayError(String message){
+    public static void displayError(String message){
         System.out.println(message);
+    }
+    /**
+     * This method displays message without going to the next line
+     * @param message
+     */
+    public static void displayMessage(String message){
+        System.out.print(message);
     }
     /**
      * This method is used to get a color according to the tile present at a
@@ -98,13 +111,51 @@ public class View{
                 default:
                     return "";
             }
+    }
+    /**
+     * This method is used to get a color according to the tile given in parameter.
+     * @param tile
+     * @return
+     */
+    private static String tileColor(Tile tile) {
+        switch (tile.color()) {
+            case BLUE:
+                return "\u001B[38;2;0;0;255m";
+            case RED:
+                return "\u001B[38;2;255;0;0m";
+            case GREEN:
+                return "\u001B[38;2;0;255;0m";
+            case ORANGE:
+                return "\u001B[38;2;255;165;0m";
+            case YELLOW:
+                return "\u001B[38;2;255;255;0m";
+            case PURPLE:
+                return "\u001B[38;2;128;0;128m";
+            default:
+                return "";
         }
+    }
     /**
      * This method is used to get a shape according to the tile present at a
      * specific position
      * */
     private static String tileShape(int row, int col, Grid grid){
         return switch (grid.get(row, col).shape()){
+            case CROSS -> "x";
+            case SQUARE -> "■";
+            case ROUND -> "o";
+            case STAR -> "*";
+            case PLUS -> "+";
+            case DIAMOND -> "◆";
+        };
+    }
+    /**
+     * This method is used to get a shape according to the tile given in parameter.
+     * @param tile
+     * @return
+     */
+    private static String tileShape(Tile tile){
+        return switch (tile.shape()){
             case CROSS -> "x";
             case SQUARE -> "■";
             case ROUND -> "o";
