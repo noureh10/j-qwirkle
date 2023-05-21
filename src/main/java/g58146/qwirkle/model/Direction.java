4613a9358a2b8cd91aedad4b92ed9d0572486f14
Nour Echaara <58146@etu.he2b.be>
2023-05-21 23:14:41 +0200

    Itération 2 - Modification de la méthode opposite

--- src/main/java/g58146/qwirkle/model/Direction.java
+++ src/main/java/g58146/qwirkle/model/Direction.java
@@ -4,36 +4,39 @@ package g58146.qwirkle.model;
  * @author Nour
  */
 public enum Direction {
-    LEFT(0,-1),RIGHT(0,1),UP(-1,0),DOWN(1,0);
+    LEFT(0, -1), RIGHT(0, 1), UP(-1, 0), DOWN(1, 0);
     private int deltaRow;
     private int deltaCol;
     /**
      * Direction constructor
      */
-    private Direction(int deltaRow, int deltaCol){
+    private Direction(int deltaRow, int deltaCol) {
         this.deltaRow = deltaRow;
         this.deltaCol = deltaCol;
     }
     /**
      * Getter for the deltaRow attribute
      */
-    public int getDeltaRow(){
+    public int getDeltaRow() {
         return deltaRow;
     }
     /**
      * Getter for the deltaCol attribute
      */
-    public int getDeltaCol(){
+    public int getDeltaCol() {
         return deltaCol;
     }
     /**
      * This method returns the opposite direction
+     *
      * @return The opposite direction
      */
-    public Direction opposite(){
-        return this == LEFT ? RIGHT:
-               this == RIGHT ? LEFT:
-               this == UP ? DOWN:
-               UP;
+    public Direction opposite() {
+        return switch (this) {
+            case LEFT -> RIGHT;
+            case RIGHT -> LEFT;
+            case UP -> DOWN;
+            case DOWN -> UP;
+        };
     }
 }
