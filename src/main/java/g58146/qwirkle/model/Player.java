88f356be6310d88832cb935f6e00cbe3fcd0adae
Nour Echaara <58146@etu.he2b.be>
2023-05-21 21:21:11 +0200

    Itération 2 - Ajout des attributs de score pour le joueur ainsi qu'un accesseur et un setter pour celui-ci

--- src/main/java/g58146/qwirkle/model/Player.java
+++ src/main/java/g58146/qwirkle/model/Player.java
@@ -1,21 +1,23 @@
 package g58146.qwirkle.model;
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 /**
  *
  * @author Nour
  */
-public class Player {
+public class Player implements Serializable {
     private String name;
     private List<Tile> tiles;
+    private int score;
     /**
      * Constructor of the player class.
      */
     public Player(String name) {
-        this.name = name;
-        this.tiles = new ArrayList<>();
+        this.name=name;
+        this.tiles=new ArrayList<>();
+        this.score=0;
     }
     /**
      * Getter for the name attribute
@@ -31,16 +33,33 @@ public class Player {
         return Collections.unmodifiableList(tiles);
     }
     /**
+     * Getter for the score attribute
+     * @return returns the score
+     */
+    public int getScore() {
+        return score;
+    }
+    /**
      * This method refills the player hand
      */
     public void refill(){
         List<Tile> t = Bag.getInstance().getRandomTiles(6-tiles.size());
+        System.out.println();
         tiles.addAll(t);
     }
-     /**
+    /**
      * This method removes tiles from the player hands
      */
     public void remove(Tile...ts){
-        tiles.removeAll(Arrays.asList(ts));
-    } 
+        for (Tile t : ts) {
+            tiles.remove(t);
+        }
+    }
+    /**
+     * This method adds score for the player
+     * @param score
+     */
+    public void addScore(int score){
+        this.score+=score;
+    }
 }
